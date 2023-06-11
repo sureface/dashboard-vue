5ff9d66507fa34fbfbeca8515633b0e7779b8228
sureface <jasurdjuraev2002@gmai.com>
2023-06-11 12:39:57 +0300

    last changes

--- src/views/Dash-board.vue
+++ src/views/Dash-board.vue
@@ -1,13 +1,17 @@
 <script setup>
 /* eslint-disable */
   import {useUserStore} from "@/store/user";
+import AddUserModal from "@/components/addUserModal.vue";
+import {computed} from "vue";
+const userStore = useUserStore()
 
 /* eslint-disable */
   const logOut = async () => {
-    const userStore = useUserStore()
     await userStore.logout()
     await this.$router.push('/')
   }
+
+  const showModal = computed(() => userStore.modalToggle)
 
   const addUser = async () => {
 
@@ -18,11 +22,12 @@
 
 <template>
   <div class="px-3 py-2 w-screen flex items-center justify-between shadow-md">
+    <add-user-modal v-if="showModal" />
     <router-link to="/Dashboard" class="text-2xl font-bold text-blue-500">
       Dashboard
     </router-link>
     <div class="flex items-center">
-      <button type="button" class="px-2 py-1 bg-blue-700 text-lg text-white rounded-lg font-semibold mr-3" @click="addUser">Add User</button>
+      <button type="button" class="px-2 py-1 bg-blue-700 text-lg text-white rounded-lg font-semibold mr-3" @click="showModal">Add User</button>
       <button type="button" class="px-2 py-1 bg-blue-500 text-lg text-white rounded-lg font-semibold" @click="logOut">Log out</button>
     </div>
   </div>
